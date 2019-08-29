a875af089673b7c0bc9a9b59b6c64413b45a8441
Nickolas Vu <nickvu71@gmail.com>
2019-08-29 02:55:12 +0000

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { 
+  host: 'flixter-nickolas-vu.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
